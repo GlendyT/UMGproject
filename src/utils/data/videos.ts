869d6d59f3966cb33c25e1151bec236b1562acb7
GlendyT <glendytuyuc@gmail.com>
2025-09-10 14:49:33 -0600

    adding the videos

--- src/utils/data/videos.ts
+++ src/utils/data/videos.ts
@@ -1,9 +1,9 @@
 export const VideosContabilidad = [
-//   {
-//     id: 1,
-//     title: "Promedio Simple",
-//     url: "it3fEkdHEPE",
-//   },
+  //   {
+  //     id: 1,
+  //     title: "Promedio Simple",
+  //     url: "it3fEkdHEPE",
+  //   },
   {
     id: 2,
     title: "Promedio Simple y Ponderado",
@@ -24,6 +24,26 @@ export const VideosContabilidad = [
     title: "Costeo de Materia Prima",
     url: "gJYITeDoImc",
   },
+  {
+    id: 6,
+    title: "Costeo de Producción",
+    url: "LUtor-8h3AQ",
+  },
+  {
+    id: 7,
+    title: "Costeo de Producción y Costeo por Órdenes",
+    url: "UX1EG9dbG6Y",
+  },
+  {
+    id: 8,
+    title: "Punto de Equilibrio",
+    url: "SfVuqtduS8A",
+  },
+  {
+    id: 9,
+    title: "Punto de Equilibrio Pt.2",
+    url: "EZCEzEBXfXE",
+  },
 ];
 
 export const VideosMatematicasDiscreta = [
@@ -36,6 +56,11 @@ export const VideosMatematicasDiscreta = [
     id: 2,
     title: "Circuitos Pt.2",
     url: "oHjHNasHjx8",
+  },
+  {
+    id: 3,
+    title: "Circuitos Pt.3",
+    url: "gk-c2YuS3_M",
   },
 ];
 
